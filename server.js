50a39814e7d750f6c1acf54a1f7d9660c99d956d
Uday Jamgade <udayjamgade1@gmail.com>
2025-01-20 22:03:13 +0530

    commit

--- server.js
+++ server.js
@@ -1,23 +1,34 @@
 import { readFile } from "fs/promises";
 import {createServer} from "https"
 import path from "path";
+import { text } from "stream/consumers";
+
+const serveFile=async(res,filepath, ContentType)=>{
+    try{
+        const data = await readFile(filepath);
+        req.writeHead(200,{ "Content-Type":ContentType});
+        req.end(data);
+    }catch (error){
+      res.writeHead(404,{"Content-Type":"text/html"})
+      res.end("404 page not found")
+    }
+     
+}
+
 
 const server = createServer(async (req,res)=>{
     if(req.method=="GET"){
         if(req.url=="/"){
-            try{
-                const data = await readFile(path.join("public","index.html"));
-                req.writeHead(200,{ "Content-Type":"text/html"});
-                req.end(data);
-            }catch (error){
-              res.writeHead(404,{"Content-Type":"text/html"})
-              res.end("404 page not found")
+            return serveFile(res, path.join("public","index.html"),"text/html")
+        }else if(req.method=="GET"){
+            if(req.url=="/style.css"){
+                return serveFile(res, path.join("public","style.css"),"text/css")
             }
         }
     }
 });
 
-server.listen(3000,()=>{
-    console.log(`server run on  http://localhost:3000`);
-    
+const PORT = 5000;
+server.listen(PORT,()=>{
+    console.log(`server run on  http://localhost:5000`);
 })
